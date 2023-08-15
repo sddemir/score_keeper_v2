3c5f6bb68f0dc253e83c687b85013e966beb6252
Sümeyye Demir <sumeyyedemir1903@gmail.com>
2023-08-15 13:57:48 +0300

    objects commit

--- scorekeeperv2.js
+++ scorekeeperv2.js
@@ -1,1 +1,10 @@
-console.log("it's working!!");
+const p1= {
+    score:0,
+    button: document.querySelector("#p1button"),
+    display: document.querySelector("#p1score")
+};
+const p2= {
+    score:0,
+    button: document.querySelector("#p2button"),
+    display: document.querySelector("#p2score")
+};
